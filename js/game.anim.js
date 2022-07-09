f2b8329dd9337a25592509a7378ab922e77bb290
神原 茉里 <maacrei5723@gmail.com>
2022-07-09 21:41:41 +0900

    Add

---
+++ js/game.anim.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// ライブラリ用のオブジェクトの作成
+if (window.game === undefined) {window.game = {}}
+if (window.game.anim === undefined) {window.game.anim = {}}
+
+(function() {
+  var _t = game.anim; // ショートカットの作成
+
+  // アニメーション実行/停止用関数
+  _t.rqstAnmFrm = function(cd) {
+    var id = (
+      window.requestAnimationFrame ||
+      window.webkitRequestAnimationFrame ||
+      window.mozRequestAnimationFrame ||
+      window.oRequestAnimationFrame ||
+      window.msRequestAnimationFrame ||
+      function(cd) {
+        return window.setTimeout(cd, 1000 / 60);
+      })(cd);
+    return id;
+  };
+
+  
+})
